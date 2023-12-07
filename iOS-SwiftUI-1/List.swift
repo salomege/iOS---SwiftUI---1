7c65f760df4c133382290f056d794e427edcf66c
salomege <90796859+salomege@users.noreply.github.com>
2023-12-07 15:35:05 -0800

    Add optional part of the homework

--- iOS-SwiftUI-1/List.swift
+++ iOS-SwiftUI-1/List.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct TableView: View {
     
     @State var person: [PersonsData] = PersonsDataList.topTen
+    @State private var showMessage = false
     
-
     var body: some View {
         NavigationView {
             
@@ -27,7 +27,7 @@ struct TableView: View {
                             Text(personData.comment)
                                 .font(.system(size:12))
                                 .foregroundColor(.secondary)
-                                
+                            
                         }
                         Spacer()
                         Text(personData.time)
@@ -35,30 +35,48 @@ struct TableView: View {
                             .foregroundColor(.secondary)
                     }
                 }
-            
+                
                 .navigationTitle("Chat")
-            .navigationBarTitleDisplayMode(.inline)
-            .accentColor(.white)
-            
+                .navigationBarTitleDisplayMode(.inline)
+                .accentColor(.white)
+                
             }
             
         }
-        Button(action: {
-            person.removeAll()
-        }, label: {
-            Text("Clear Chat")
-                .font(.system(size: 20))
+        HStack(spacing: 10) {
+            Button(action: {
+                showMessage = true
+                person.removeAll()
+            }, label: {
+                Text("Clear Chat")
+                    .font(.system(size: 20))
+                
+            })
+            .foregroundColor(.white)
+            .frame(width:150, height: 45)
+            .background(Color(red: 36/255, green: 39/255, blue:96/255))
+            .cornerRadius(6)
+            .alert(isPresented: $showMessage) {
+                Alert(title: Text("Data is removed"))
+            }
+            Button(action: {
+                person = PersonsDataList.topTen
+            }, label: {
+                Text("Restore Chat")
+                    .font(.system(size: 20))
+                
+            })
+            .foregroundColor(.white)
+            .frame(width:150, height: 45)
+            .background(Color(red: 36/255, green: 39/255, blue:96/255))
+            .cornerRadius(6)
             
-        })
-        .foregroundColor(.white)
-        .frame(width:224, height: 45)
-        .background(Color(red: 36/255, green: 39/255, blue:96/255))
-        .cornerRadius(6)
-    }
-        
-        
         }
-        
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
 
 
 
